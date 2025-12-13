3a9ff21171c58d314dabc3f316ba8d85ae848ac8
Cognitio+ <132303904+cognitioplus@users.noreply.github.com>
2025-12-13 08:16:12 +0800

    Create DDRiVER.tsx

---
+++ src/pages/DDRiVER.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DDRiVERWidget from "@/components/DDRiVERWidget";
+
+const DDRiVERPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-800 flex items-center justify-center">
+      <DDRiVERWidget />
+    </div>
+  );
+};
+
+export default DDRiVERPage;
